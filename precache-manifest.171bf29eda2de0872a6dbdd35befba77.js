aa188c50d8897c7a80fce12ee80a82354a0026bc
Rodrigo Gomez <Rodrigo@MacBook-Air-de-Rodrigo.local>
2020-01-17 09:04:55 -0300

    Updates

---
+++ precache-manifest.171bf29eda2de0872a6dbdd35befba77.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10dd8780e4429f7820a2e3e932b60e31",
+    "url": "/Asistente-De-Cultivo/index.html"
+  },
+  {
+    "revision": "8abd57c0c8d8f9973c3f",
+    "url": "/Asistente-De-Cultivo/static/css/2.d13641dc.chunk.css"
+  },
+  {
+    "revision": "db1ad09ffc807b2a3f3c",
+    "url": "/Asistente-De-Cultivo/static/css/main.620597fb.chunk.css"
+  },
+  {
+    "revision": "8abd57c0c8d8f9973c3f",
+    "url": "/Asistente-De-Cultivo/static/js/2.ffb4a060.chunk.js"
+  },
+  {
+    "revision": "db1ad09ffc807b2a3f3c",
+    "url": "/Asistente-De-Cultivo/static/js/main.e80a4c5b.chunk.js"
+  },
+  {
+    "revision": "1b5965595d2f43616036",
+    "url": "/Asistente-De-Cultivo/static/js/runtime-main.ae36a367.js"
+  },
+  {
+    "revision": "7617cb2073d502c7a8aff6d1dd0e853b",
+    "url": "/Asistente-De-Cultivo/static/media/apple cookies.7617cb20.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Asistente-De-Cultivo/static/media/logo.25bf045c.svg"
+  }
+]);
