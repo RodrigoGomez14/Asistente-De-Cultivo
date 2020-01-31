c952fdf854508c94cf79e995e913f9aad211e6b3
Rodrigo Gomez <Rodrigo@MacBook-Air-de-Rodrigo.local>
2020-01-31 16:58:42 -0300

    0.8.9.1

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -60,6 +60,7 @@ export const LoginPage=({history})=> {
         <Grid container component="main" className={classes.root}>
             <CssBaseline />
             <Grid item xs={false} sm={4} md={7} className={classes.image}>
+              <img src={foto} alt="" className={classes.img}/>
             </Grid>
             <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
             <div className={classes.paper}>
